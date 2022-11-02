0682d8a39e74d75b24314d8ffa12b09cea179643
Saphal Pant <saphalpant7@gmail.com>
2022-11-02 18:19:03 +0545

    calculte the mean value of each colums

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include <windows.h>
 
 
-void displayColumnSummary(int);
+void displayColumnSummary(int col[100][100]);
 void calcStDev(int);
 void handleMissingValues(int);
 int variableType(int);
@@ -45,20 +45,15 @@ int main() {
 
 
 
-        while (!feof(fptr))
+    while (!feof(fptr))
     {
         fgets(row, 1500, fptr);
-
         token = strtok(row, ",");
-        // printf("%s\n",token);
-        
+
         while(token != NULL)
         {
             val = atoi(token);
-            // printf("%d\n",val);
             data[r][c] = val;
-            // printf("colums:%d",c);
-            // printf("rows:%d\n",r);
             c++;
             token = strtok(NULL, ",");
         }
@@ -66,33 +61,70 @@ int main() {
         r++;        
     }
 
-    // printf("%d\n", data[1][0]);
+// displays the data in the array
+    // for(int i=0; i<100; i++){
+    //     for(int j=0; j<15; j++){
+    //         printf("%d\t", data[i][j]);
+    // }
+    // printf("\n");
+    // }
 
-    for(int i=0; i<100; i++){
-        for(int j=0; j<15; j++){
-            printf("%d\t", data[i][j]);
-    }
-    printf("\n");
-    }
+displayColumnSummary(data);
 
 
 
+
+
+
+
+//close the file
     fclose(fptr);
     return 0;
 }
 
-void displayColumnSummary(int colIndex){
+void displayColumnSummary(int col[100][100]){
+    int i,j;
+    float sum=0;
+    float avg=0;
+    char opt;
+
+    printf("Please Eenter a method to fil the missing values:\nA.) Mean\nB.) Median\nC.) Mode\n\n=>");
+    scanf("%c", &opt);
+
+    switch(opt){
+
+    case 'A':
+        for(i=0; i<15; i++){
+            for(j=0; j<100; j++){
+                sum = sum + col[j][i];
+            }
+        avg = sum / 100;
+        printf("The average of colum %d is %.2f\n", i+1, avg);
+        sum=0;
+        avg=0;
+        }
+        break;
+
+    case 'B':
+
+        break;
+
+    case 'C':
+        break;
+
+    }
+}
+
+
+void calcStDev(int data){
 
 }
 
-void calcStDev(int colIndex){
+void handleMissingValues(int data){
+
 
 }
 
-void handleMissingValues(int colIndex){
-
-}
-
-int variableType(int colIndex){
+int variableType(int data){
 
 }
