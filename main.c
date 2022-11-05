b9c1e7a3d0e9c5d306eee5608f5ea8c7851be372
Saphal Pant <saphalpant7@gmail.com>
2022-11-05 13:19:32 +0545

    Replaces all the zeros in every column with the mean value of that column

--- main.c
+++ main.c
@@ -2,13 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <ctype.h>
 #include <windows.h>
+#include "strsep.h"
 
 
 void displayColumnSummary(int col[100][100]);
+int nullFinder(int col[100][100], int colNum);
+void handleMissingValues(int col[100][100], int colNum, int avg);
 void calcStDev(int);
-void handleMissingValues(int);
 int variableType(int);
 
 
@@ -18,6 +19,7 @@ int main() {
 
     char path[200];
     int data[100][100];
+
     char *token;
     int val, rowNum, colNum, nullNum;
     int r=0;
@@ -48,14 +50,13 @@ int main() {
     while (!feof(fptr))
     {
         fgets(row, 1500, fptr);
-        token = strtok(row, ",");
-
-        while(token != NULL)
+        char * cpy = strdup(row);
+         
+        while( (token = strsep(&cpy, ",")) != NULL)
         {
             val = atoi(token);
             data[r][c] = val;
             c++;
-            token = strtok(NULL, ",");
         }
         c=0;
         r++;        
@@ -84,25 +85,34 @@ displayColumnSummary(data);
 
 void displayColumnSummary(int col[100][100]){
     int i,j;
-    float sum=0;
-    float avg=0;
+    int sum=0;
+    int avg=0;
     char opt;
+    int indexCount;
 
-    printf("Please Eenter a method to fil the missing values:\nA.) Mean\nB.) Median\nC.) Mode\n\n=>");
+    printf("Please Eenter a method to fil the missing values:\nA.) Mean\nB.) Median\n\n =>");
     scanf("%c", &opt);
 
     switch(opt){
 
     case 'A':
         for(i=0; i<15; i++){
+        nullFinder(col,i);
             for(j=0; j<100; j++){
                 sum = sum + col[j][i];
             }
-        avg = sum / 100;
-        printf("The average of colum %d is %.2f\n", i+1, avg);
+        avg = sum / (j-indexCount);
+        printf("The average of colum %d is %d\n\n", i+1, avg);
+        handleMissingValues(col, i, avg);
         sum=0;
         avg=0;
         }
+            for(int i=0; i<100; i++){
+        for(int j=0; j<15; j++){
+            printf("%d\t", col[i][j]);
+    }
+    printf("\n");
+    }
         break;
 
     case 'B':
@@ -115,15 +125,33 @@ void displayColumnSummary(int col[100][100]){
     }
 }
 
+int nullFinder(int col[100][100], int colNum){
+    int indexCount = 0;
+
+    for(int i=0; i<100; i++){
+        if(col[i][colNum]==0){
+            indexCount++;
+        }
+    }
+
+    return indexCount;
+}
+
+void handleMissingValues(int col[100][100], int colNum, int avg){
+    for(int i=0; i<100; i++){
+        if(col[i][colNum]==0){
+            col[i][colNum] = avg;
+    }
+}
+
+}
+
 
 void calcStDev(int data){
 
 }
 
-void handleMissingValues(int data){
 
-
-}
 
 int variableType(int data){
 
